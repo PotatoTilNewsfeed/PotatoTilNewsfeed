5b7d6ed7d52a6a13bc3d5f79585581cf3aae8bd1
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-02-15 07:56:12 +0900

    :sparkles: 게시물 생성 url 수정

--- src/main/java/com/example/potatotilnewsfeed/domain/til/controller/TilController.java
+++ src/main/java/com/example/potatotilnewsfeed/domain/til/controller/TilController.java
@@ -45,7 +45,7 @@ public class TilController {
         this.tilService = tilService;
     }
 
-    @PostMapping("/v1/tils/all")
+    @PostMapping("/v1/tils")
     public ResponseEntity<ResponseDto<List<TilData>>> createTil(@RequestBody TilDto tilDto,
         HttpServletResponse response) {
         log.info("TIL 생성 API");
